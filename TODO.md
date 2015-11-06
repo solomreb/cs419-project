0d5cc9b593604730c04e71bacaf1316b6e66ed86
arbarnett <barnetal@onid.oregonstate.edu>
2015-11-06 14:31:54 -0700

    Query_table almost done
    Just needs paginated results : it is not pretty, but it works!

--- TODO.md
+++ TODO.md
@@ -6,12 +6,24 @@
     * Sames as above: can't visualize this one 
 * create_table.py
     * createColumns()
-* Do we want to list the options of Types? Or, are we assuming that the user knows the types available
-* It might be easiest for the databast queries to do it this way:
-    * create the table with just the name first CREATE TABLE table_name
-    * then, create the columns using the ALTER TABLE command each time a column is entered? (http://www.techonthenet.com/sql/tables/alter_table.php)
+		* Do we want to list the options of Types? Or, are we assuming that the user knows the types available
+	* It might be easiest for the databast queries to do it this way:
+    	* create the table with just the name first CREATE TABLE table_name
+    	* then, create the columns using the ALTER TABLE command each time a column is entered? (http://www.techonthenet.com/sql/tables/alter_table.php)
 * constants / constraints
-    * Maybe we set a max number of columns?
-    * Maybe we don't allow users to edit structure (yet)
-        * if we have time, we can add this functionality, but for now it could just be, what you've set up is what you get??
-    *Instead of edit structure, maybe "query table", in which user can enter their own query?
+	* Maybe we set a max number of columns?
+
+
+
+Queries for Viewing table:
+
+-Get all the column names:
+	SELECT `COLUMN_NAME` 
+	FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+	WHERE `TABLE_SCHEMA`='yourdatabasename' 
+    AND `TABLE_NAME`='yourtablename';
+-run the query and store into the variable 'rows':
+	http://stackoverflow.com/questions/28981770/store-sql-result-in-a-variable-in-python
+
+	results will look like this: [['Bob', '9123 4567'], ['Janet', '8888 8888']]
+		-this is a query to return name and phone number
--- dashboard_table.py
+++ dashboard_table.py
@@ -26,7 +26,7 @@ def table_menu(username, database, table):
         screen.addstr(5, 5, 'What would you like to do with this table?', curses.A_BOLD | curses.A_UNDERLINE)
         screen.addstr(6, 5, table, curses.A_BOLD)
         screen.addstr(8, 5, 'View', choices[0])
-        screen.addstr(11, 5, 'Edit Structure', choices[1])
+        screen.addstr(11, 5, 'Query the Table', choices[1])
         screen.addstr(14, 5, 'Insert', choices[2])
         screen.addstr(17, 5, 'Drop', choices[3])
 
@@ -44,7 +44,7 @@ def table_menu(username, database, table):
         if selection == 0:
             view_table(username, database, table)
         elif selection == 1:
-            edit_table(username, database, table)
+            query_table(username, database, table)
         elif selection == 2:
             insert_table(username, database, table)
         elif selection == 3:
@@ -148,14 +148,62 @@ def insert_table(username, database, table):
     screen.refresh()
 
 
-def edit_table(username, database, table):
+def query_table(username, database, table):
     """
     :param username:
     :param database:
     :param table:
     :return:
     """
+
+    #Get the query
     screen = curses.initscr()
     screen.clear()
-    screen.addstr(6, 5, "THIS NEEDS TO BE DONE", curses.A_BOLD)
+    screen.addstr(5, 5, "Enter your query line:", curses.A_BOLD)
     screen.refresh()
+    
+    #IS 50 CHARS LONG ENOUGH FOR A QUERY???????????????????????????????
+    query = screen.getstr(6, 5, 50)
+
+    #run the query
+    """RUN THIS QUERY TO THE TABLE"""
+
+    #Display results
+    screen.clear()
+    screen.addstr(1,1, "Results:", curses.A_BOLD)
+    
+    """Here we run a query against the table listed in the parameter
+    to find all the column names, See TO-DO file for information
+    on these database queries"""
+    #for testing, this is hard-coded
+    columns = ['col1', 'col2', 'col3','col4']
+    
+    x = 1 #starting x value
+
+    for name in columns:
+        screen.addstr(3, x, name)
+        x += 10
+
+    screen.refresh()
+
+
+    """Here we run the query that the user entered, stored in the
+    variable 'query', and store the results of the query into 'rows'"""
+    #run the query
+    #for testing, the results are hardcoded
+    rows = [['Bob', '9123 4567','home','A'], ['Janet', '8888 8888', 'cell', 'B']]
+    y = 5 #starting y
+    x=1 #starting x
+
+    for row in rows:
+        for result in row:
+            screen.addstr(y, x, result)
+            x += 10
+        y += 1 #move to next line
+        x = 1 #reset x
+
+    screen.refresh()
+
+
+
+
