163cec42219fc6cf118367032490fee528a04ad8
solomreb <solomreb@onid.oregonstate.edu>
2015-11-17 17:05:40 -0800

    can now add new database!

--- model_base.py
+++ model_base.py
@@ -3,13 +3,6 @@ from peewee import Model, PostgresqlDatabase
 psql_db = PostgresqlDatabase('cs419', user='vagrant')
 
 
-def before_request_handler():
-    database.connect()
-
-
-def after_request_handler():
-    database.close()
-
 
 class PostgresqlModel(Model):
     class Meta:
--- utility_database.py
+++ utility_database.py
@@ -1,16 +1,29 @@
 from peewee import IntegrityError
 from model_database import DatabaseModel
+import psycopg2
+import psycopg2.extensions
 
+def connect_default_db():
+
+    conn = psycopg2.connect(dbname='postgres', user='postgres')
+    conn.set_isolation_level(psycopg2.extensions.ISOLATION_LEVEL_AUTOCOMMIT)
+    return conn
 
 def add_database(user, database_name):
-    """
-    :param user:
-    :param database_name:
-    :return:
-    """
-    try:
-        database = DatabaseModel.create(user=user, name=database_name)
-    except IntegrityError:
-        return False
 
-    return database
+    conn = connect_default_db()
+    cursor = conn.cursor()
+
+    """
+    find_query = "SELECT 1 FROM pg_database WHERE datname = \'%s\'" % (database_name)
+    result = cursor.execute(find_query)
+    if result.rowcount > 0:
+        print 'Database already exists.'
+        return False
+    else:
+    """
+    create_query = "CREATE DATABASE " + database_name
+    cursor.execute(create_query)
+    conn.commit()
+    conn.close()
+    return True
